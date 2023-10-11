45b3b5e0bcbcf8f4c8145f41dc6494d9995ba26d
APlima16 <134547485+APlima16@users.noreply.github.com>
2023-10-11 11:12:03 -0300

    ana

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
     mostrarMensagemBtn.addEventListener("click", function () {
         mensagem.innerHTML = "Queridos professores, hoje é o dia de celebrar vocês, " +
-        "Não tem palavras no mundo capaz de agradecer vocês por tudo que fazem por nós" +
-        "Nesse dia tão especial, quero que vocês saibam da sua importância em nossa vida" +
-        "São vocês, professores, que nos moldam para nossa vida, que nos ensinam tudo, e nos preparam para o mundo" +
-        "Minha gratidão a vocês é imensa, e minha admiração pelo seu trabalho aumenta a cada dia" +
+        "não tem palavras no mundo capaz de agradecer vocês por tudo que fazem por nós." +
+        "Nesse dia tão especial, quero que vocês saibam da sua importância em nossa vida." +
+        "São vocês, professores, que nos moldam para nossa vida, que nos ensinam tudo, e nos preparam para o mundo," +
+        "minha gratidão a vocês é imensa, e minha admiração pelo seu trabalho aumenta a cada dia" +
         "Amo todos vocês, guerreiros sem capa, que nos inspirm todo os dias." +
         "Feliz Dia dos Professores!";
     });
